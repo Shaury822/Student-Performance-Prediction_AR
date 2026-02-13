79a8c436b8eef72dc17b3b6d2a1866edf4c95d2f
Shaury822 <shouryachaubey8@gmail.com>
2026-02-13 15:17:24 +0530

    app.py

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ Original file is located at
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import pickle
+
+import streamlit as st
+import joblib
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
